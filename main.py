6cb4baedabdfc8351bb1330b5ed617eee0e7eaa5
yourwifeisdead <gamesrud@yandex.ru>
2020-05-03 16:25:52 +0200

    Added wind mode activation

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 from Boid import Boid
 import pygame
 import random
-
+import numpy as np
 
 pygame.init()
-
+pygame.font.init()
 width, height = 1280, 720
 screen = pygame.display.set_mode((width, height))
+font = pygame.font.SysFont('Times New Roman', 18)
 white = (255, 255, 255)
 
 clock = pygame.time.Clock()
 
-boids = [Boid(random.randint(0, 1280), random.randint(0, 720), 10, 10) for _ in range(30)]
+boids = [Boid(random.randint(0, 1280), random.randint(0, 720), 10, 10)
+         for _ in range(30)]
 running = True
+
+is_wind = False
+wind_deviation = np.random.rand(2)
+
 
 while running:
     screen.fill(white)
     for b in boids:
         b.edges()
         b.flock(boids)
+        if is_wind:
+            b.wind_effect(wind_deviation)
         b.update()
         b.draw(screen)
 
@@ -30,7 +38,13 @@ while running:
             if event.key == pygame.K_ESCAPE:
                 running = False
 
+            if event.key == pygame.K_SPACE:
+                is_wind = not is_wind
+                wind_deviation = np.random.rand(2)
+
     clock.tick(60)
+    text = font.render(f"Random Wind: {is_wind}", True, (0, 0, 0))
+    screen.blit(text, (width - 0.15 * width, 0.1 * height))
     pygame.display.update()
 
 pygame.quit()
