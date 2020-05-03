509cb6babbe1565f658b8b4e6475549d8d6a50cd
yourwifeisdead <gamesrud@yandex.ru>
2020-05-03 18:54:04 +0200

    Creating obstacles added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from Boid import Boid
+from Obstacle import Obstacle
 import pygame
 import random
 import numpy as np
@@ -14,6 +15,7 @@ clock = pygame.time.Clock()
 
 boids = [Boid(random.randint(0, 1280), random.randint(0, 720), 10, 10)
          for _ in range(30)]
+obstacles = []
 running = True
 
 is_wind = False
@@ -23,11 +25,14 @@ while running:
     screen.fill(white)
     for b in boids:
         b.edges()
-        b.flock(boids)
+        b.flock(boids, obstacles)
         if is_wind:
             b.wind_effect(wind_deviation)
         b.update()
         b.draw(screen)
+
+    for o in obstacles:
+        o.draw(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -40,6 +45,10 @@ while running:
             if event.key == pygame.K_SPACE:
                 is_wind = not is_wind
                 wind_deviation = np.random.randint(-1, 1, 2)
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            pos = pygame.mouse.get_pos()
+            obstacles.append(Obstacle(pos[0], pos[1], 10, 10))
 
     clock.tick(60)
     text = font.render(f"Random Wind: {is_wind}", True, (0, 0, 0))
